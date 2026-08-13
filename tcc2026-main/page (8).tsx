30b1865fd7da1bbf9f08182b2fc434adbb898abb
davitavares77 <tavarezdavi146@gmail.com>
2026-08-13 18:24:18 -0300

    fix: remove arquivos page duplicados do rastreamento

--- tcc2026-main/page (8).tsx
+++
@@ -1,8 +0,0 @@
-import Image from "next/image";
-import SignUpHookForm from "./_components/sign-up-hook-form";
-import Cultos from "./_components/cultos"
-import Tabela from "./_components/tabela"
-
-export default function Home() {
-  return <SignUpHookForm/>; 
-}
